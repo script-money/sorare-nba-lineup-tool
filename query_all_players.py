b848a176717f1b3cba96f8292b17cf8b488e0cce
script-money <hello@script.money>
2023-01-11 18:04:01 +0800

    ♻️ write query all player to async version

--- query_all_players.py
+++ query_all_players.py
@@ -1,58 +1,71 @@
-import asyncio
 import json
+from asyncio import Task, run, wait, create_task
 
-from gql import Client, gql
+from gql import Client, gql, client
 from gql.transport.aiohttp import AIOHTTPTransport
-from types_ import team_slugs
+from types_ import NBAPlayer, team_slugs
 from datetime import datetime
 from pytz import timezone
 
+with open("query/NBAPlayersByTeamQuery.graphql") as f:
+    playersQuery = gql(f.read())
+
+with open("query/NBAPlayersBySlugsQuery.graphql") as g:
+    playerDataQuery = gql(g.read())
+
 
 async def main():
-
-    transport = AIOHTTPTransport(
-        url="https://api.sorare.com/sports/graphql",
-        # headers = {"Authorization": "Bearer <TheUserAccessToken>"}
-    )
-
-    async with Client(transport=transport) as session:
-        with open("query/NBAPlayersByTeamQuery.graphql") as f:
-            playersQuery = gql(f.read())
-
-        with open("query/NBAPlayersBySlugsQuery.graphql") as g:
-            playerDataQuery = gql(g.read())
-
-        all_cards_info = []
+    async with Client(
+        transport=AIOHTTPTransport(
+            url="https://api.sorare.com/sports/graphql",
+            # headers = {"Authorization": "Bearer <TheUserAccessToken>"}
+        )
+    ) as session:
+        all_cards_info: list[NBAPlayer] = []
+        task_list: list[Task] = []
         for team_slug in team_slugs:
-            playersResult = await session.execute(
-                playersQuery, variable_values={"input": team_slug}
-            )
-            players_in_a_team: list[str] = list(
-                map(lambda i: i["slug"], playersResult["nbaTeam"]["players"])
-            )
+            task: Task = create_task(query_task(team_slug, session))
+            task_list.append(task)
 
-            players_in_a_team: list[str] = list(
-                set(players_in_a_team)
-                - set(
-                    ["ibou-badji-20021013"]
-                )  # ibou badji has no data, query will cause error
-            )
+        done: set[Task[list[NBAPlayer]]] = (await wait(task_list, timeout=None))[0]
 
-            playerDataResult = await session.execute(
-                playerDataQuery, variable_values={"input": players_in_a_team}
-            )
-
-            for playerData in playerDataResult["nbaPlayers"]:
-                all_cards_info.append(playerData)
-
-            print(f"Query {team_slug} done.")
+        for future in done:
+            all_cards_info.extend(future.result())
 
         today: datetime = datetime.now(timezone("US/Eastern"))
         today_str: str = today.strftime("%Y-%m-%d")
 
         with open(f"data/all-players-data-{today_str}.json", "w") as f:
             json.dump(all_cards_info, f, indent=4)
-            print("all player slug saved in data folder")
+            print("all player data saved in data folder")
 
 
-asyncio.run(main())
+async def query_players_in_a_team_task(team_slug: str, session) -> list[str]:
+    playersResult = await session.execute(
+        playersQuery, variable_values={"input": team_slug}
+    )
+    players_in_a_team: list[str] = list(
+        map(lambda i: i["slug"], playersResult["nbaTeam"]["players"])
+    )
+
+    players_in_a_team: list[str] = list(
+        set(players_in_a_team)
+        - set(["ibou-badji-20021013"])  # ibou badji has no data, query will cause error
+    )
+    return players_in_a_team
+
+
+async def query_player_data_task(players_in_a_team, session: client.AsyncClientSession):
+    playerDataResult = await session.execute(
+        playerDataQuery, variable_values={"input": players_in_a_team}
+    )
+    return [playerData for playerData in playerDataResult["nbaPlayers"]]
+
+
+async def query_task(team_slug, session: client.AsyncClientSession):
+    players_in_a_team = await query_players_in_a_team_task(team_slug, session)
+    playerDatas = await query_player_data_task(players_in_a_team, session)
+    return playerDatas
+
+
+run(main())
